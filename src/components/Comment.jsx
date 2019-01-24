c01c1eeba0e3c3233a9f7846e4cc7ac70b38881d
Aleksander Chromik <achromik@gmail.com>
2019-01-24 13:46:09 +0100

    Add: voteDown and delete comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { voteUpComment } from "../actions/actions";
+import {
+  voteUpComment,
+  voteDownComment,
+  removeComment
+} from "../actions/actions";
 
-const CommentRaw = ({ text, votes, id, voteUpComment }) => (
+const CommentRaw = ({
+  text,
+  votes,
+  id,
+  voteUpComment,
+  voteDownComment,
+  removeComment
+}) => (
   <li>
     {text} <span>votes: {votes}</span>
     <button onClick={() => voteUpComment(id)}>Vote Up</button>
+    <button onClick={() => voteDownComment(id)}>Vote Down</button>
+    <button onClick={() => removeComment(id)}>Delete</button>
   </li>
 );
 
 export const Comment = connect(
   null,
-  { voteUpComment }
+  { voteUpComment, voteDownComment, removeComment }
 )(CommentRaw);
