af6b6f72a5501d6e5a7b37c7a498c32fbf7ef165
Aleksander Chromik <achromik@gmail.com>
2019-01-24 09:42:33 +0100

    Fix: store export type

--- src/store/store.js
+++ src/store/store.js
@@ -2,4 +2,6 @@ import { createStore } from "redux";
 
 import { rootReducer } from "../reducers/rootReducer";
 
-export const store = createStore(rootReducer);
+const store = createStore(rootReducer);
+
+export default store;
